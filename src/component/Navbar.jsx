b84453e6b9c43582b4a4a58acd65ccb3b74d7ecf
HaidaraSalameh9 <haidarhsalamh9@gmail.com>
2026-09-17 17:34:36 +0530

    fix react router navigation

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,8 +4,6 @@ import { useClerk, useUser, UserButton } from '@clerk/react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FaBook } from 'react-icons/fa';
 
-
-
 const Navbar = () => {
     const navLinks = [
         { name: 'Home', path: '/' },
@@ -14,14 +12,11 @@ const Navbar = () => {
         { name: 'About', path: '/' },
     ];
 
-
-
     const [isScrolled, setIsScrolled] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const { openSignIn } = useClerk();
     const { user } = useUser();
-
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -34,98 +29,214 @@ const Navbar = () => {
                 setIsScrolled(true);
             }
         };
+
         handleScroll();
 
         window.addEventListener("scroll", handleScroll);
+
         return () => window.removeEventListener("scroll", handleScroll);
     }, [location.pathname]);
 
     return (
-
-        <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-3 md:px-12 lg:px-18 xl:px-25 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-3" : "py-4 md:py-4"}`}>
+        <nav
+            className={`fixed top-0 left-0 w-full flex items-center justify-between px-3 md:px-12 lg:px-18 xl:px-25 transition-all duration-500 z-50 ${
+                isScrolled
+                    ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-3"
+                    : "py-4 md:py-4"
+            }`}
+        >
 
             {/* Logo */}
-            <a href="/" className="flex items-center gap-2">
-                <img src={logo} className={`h-24 w-24 transition-transform duration-300 ${isScrolled ? "scale-90" : "scale-100"}`} />
-            </a>
+            <Link to="/" className="flex items-center gap-2">
+                <img
+                    src={logo}
+                    className={`h-24 w-24 transition-transform duration-300 ${
+                        isScrolled ? "scale-90" : "scale-100"
+                    }`}
+                />
+            </Link>
 
             {/* Desktop Nav */}
             <div className="hidden md:flex items-center gap-4 lg:gap-8">
+
                 {navLinks.map((link, i) => (
-                    <a key={i} href={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-gray-700" : "text-white"}`}>
+                    <Link
+                        key={i}
+                        to={link.path}
+                        className={`group flex flex-col gap-0.5 ${
+                            isScrolled ? "text-gray-700" : "text-white"
+                        }`}
+                    >
                         {link.name}
-                        <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
-                    </a>
+
+                        <div
+                            className={`${
+                                isScrolled ? "bg-gray-700" : "bg-white"
+                            } h-0.5 w-0 group-hover:w-full transition-all duration-300`}
+                        />
+                    </Link>
                 ))}
-                {user && <Link to={"/dashboard"}>
-                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
-                        Dashboard
-                    </button>
-                </Link>
-                }
+
+                {user && (
+                    <Link to="/dashboard">
+                        <button
+                            className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
+                                isScrolled ? 'text-black' : 'text-white'
+                            } transition-all`}
+                        >
+                            Dashboard
+                        </button>
+                    </Link>
+                )}
 
             </div>
 
             {/* Desktop Right */}
             <div className="hidden md:flex items-center gap-4">
-                <svg className={`h-6 w-6 text-white transition-all duration-500 ${isScrolled ? "invert" : ""}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+
+                <svg
+                    className={`h-6 w-6 text-white transition-all duration-500 ${
+                        isScrolled ? "invert" : ""
+                    }`}
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                >
                     <circle cx="11" cy="11" r="8" />
-                    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+                    <line
+                        x1="21"
+                        y1="21"
+                        x2="16.65"
+                        y2="16.65"
+                    />
                 </svg>
-                {user ?
-                    (<UserButton>
+
+                {user ? (
+                    <UserButton>
                         <UserButton.MenuItems>
-                            <UserButton.Action labelIcon={<FaBook />} label='My Booking' onClick={() => navigate("/my-bookings")} />
+                            <UserButton.Action
+                                labelIcon={<FaBook />}
+                                label="My Booking"
+                                onClick={() => navigate("/my-bookings")}
+                            />
                         </UserButton.MenuItems>
-                    </UserButton>) :
-                    (<button onClick={openSignIn} className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
+                    </UserButton>
+                ) : (
+                    <button
+                        onClick={openSignIn}
+                        className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${
+                            isScrolled
+                                ? "text-white bg-black"
+                                : "bg-white text-black"
+                        }`}
+                    >
                         Login
-                    </button>)
-                }
+                    </button>
+                )}
 
             </div>
 
             {/* Mobile Menu Button */}
-            <div className="flex  items-center gap-3 md:hidden">
-                {user && (<UserButton>
-                    <UserButton.MenuItems>
-                        <UserButton.Action labelIcon={<FaBook />} label='My Booking' onClick={() => navigate("/my-bookings")} />
-                    </UserButton.MenuItems>
-                </UserButton>)}
-                <svg onClick={() => setIsMenuOpen(!isMenuOpen)} className={`text-white h-6 w-6 cursor-pointer ${isScrolled ? "invert" : ""}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+            <div className="flex items-center gap-3 md:hidden">
+
+                {user && (
+                    <UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action
+                                labelIcon={<FaBook />}
+                                label="My Booking"
+                                onClick={() => navigate("/my-bookings")}
+                            />
+                        </UserButton.MenuItems>
+                    </UserButton>
+                )}
+
+                <svg
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    className={`text-white h-6 w-6 cursor-pointer ${
+                        isScrolled ? "invert" : ""
+                    }`}
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                >
                     <line x1="4" y1="6" x2="20" y2="6" />
                     <line x1="4" y1="12" x2="20" y2="12" />
                     <line x1="4" y1="18" x2="20" y2="18" />
                 </svg>
+
             </div>
 
             {/* Mobile Menu */}
-            <div className={` fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                <button className="bg-white! text-black! absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                    <svg className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                        <line x1="18" y1="6" x2="6" y2="18" />
-                        <line x1="6" y1="6" x2="18" y2="18" />
+            <div
+                className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${
+                    isMenuOpen
+                        ? "translate-x-0"
+                        : "-translate-x-full"
+                }`}
+            >
+
+                <button
+                    className="bg-white! text-black! absolute top-4 right-4"
+                    onClick={() => setIsMenuOpen(false)}
+                >
+                    <svg
+                        className="h-6 w-6"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        viewBox="0 0 24 24"
+                    >
+                        <line
+                            x1="18"
+                            y1="6"
+                            x2="6"
+                            y2="18"
+                        />
+                        <line
+                            x1="6"
+                            y1="6"
+                            x2="18"
+                            y2="18"
+                        />
                     </svg>
                 </button>
 
                 {navLinks.map((link, i) => (
-                    <a key={i} href={link.path} onClick={() => setIsMenuOpen(false)}>
+                    <Link
+                        key={i}
+                        to={link.path}
+                        onClick={() => setIsMenuOpen(false)}
+                    >
                         {link.name}
-                    </a>
+                    </Link>
                 ))}
 
-                {user && <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
-                    Dashboard
-                </button>}
+                {user && (
+                    <Link
+                        to="/dashboard"
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
+                            Dashboard
+                        </button>
+                    </Link>
+                )}
 
-
-                {!user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
-                    Login
-                </button>}
+                {!user && (
+                    <button
+                        onClick={openSignIn}
+                        className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500"
+                    >
+                        Login
+                    </button>
+                )}
 
             </div>
-        </nav>
 
+        </nav>
     );
 };
 
